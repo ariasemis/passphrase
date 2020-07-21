8156aed3aa11e6f3b8acb90121b1297ea245a6e6
Arias Emilio <ariasemis@users.noreply.github.com>
2020-07-21 15:32:35 -0300

    changed file reading and small fixes

--- README.md
+++ README.md
@@ -4,18 +4,12 @@ Inspired by <https://www.eff.org/dice>
 
 ## Usage
 
-1. Install .NET Core 3.0 or higher if you don't have it already
-2. Restore dependencies
-  ```
-  dotnet tool restore
-  dotnet paket restore
-  ```
-3. Run script
-  ```
-  dotnet fsi passphrase.fsx
-  ```
+```
+dotnet fsi passphrase.fsx
+```
 
 ## License
 
-The wordlist included was created by Electronic Frontier Foundation, and is distributed under the Creative Commons Attribution 3.0.
+"eff_large_wordlist" by Electronic Frontier Foundation, used under the Creative Commons Attribution 3.0.
+
 The script itself is licensed under the MIT license.
--- passphrase.fsx
+++ passphrase.fsx
@@ -1,29 +1,27 @@
-#r @"packages/FSharp.Data/lib/netstandard2.0/FSharp.Data.dll"
-
 open System
-open FSharp.Data
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let csv = new CsvProvider<"wordlist.csv", HasHeaders = false>()
-
-let find n =
-    csv.Rows |> Seq.find (fun r -> r.Column1 = n)
-
-let word n = (find n).Column2
+let words =
+    IO.File.ReadAllLines("eff_large_wordlist.csv")
+    |> Seq.map (fun r -> r.Split(','))
+    |> Seq.map (fun r -> int r.[0], r.[1])
+    |> Map.ofSeq
 
 let rnd = new Random()
 
 let dice() = rnd.Next(1, 7)
 
 let num digits = 
-    digits |> List.fold (fun acc n -> acc * 10 + n) 0
+    digits |> List.fold (fun a n -> a * 10 + n) 0
 
 let roll() = 
     [1..5] |> List.map (fun _ -> dice()) |> num
 
 let passphrase =
-    [1..6] |> List.map (fun _ -> roll()) |> List.map word |> String.concat " "
+    [1..6]
+    |> List.map (fun _ -> roll())
+    |> List.map (fun n -> words.[n])
+    |> String.concat " "
 
-printfn "your passphrase is:"
-printfn "%s%s" Environment.NewLine passphrase
+printfn "%s%s" passphrase Environment.NewLine
